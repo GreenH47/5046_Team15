02ba6a6e119bbefb6f4a19b893f04903c4003d4a
GreenH <huangshixin47@gmail.com>
2023-05-08 20:20:23 +1000

    deploy db and livedata

--- Vitality/app/src/main/java/com/example/vitality/fragment/DashboardFragment.java
+++ Vitality/app/src/main/java/com/example/vitality/fragment/DashboardFragment.java
@@ -12,10 +12,15 @@ import androidx.navigation.Navigation;
 
 import com.example.vitality.databinding.ActivityDashboardBinding;
 import com.example.vitality.databinding.ActivityDietLogBinding;
+import com.example.vitality.entity.Diet;
+import com.example.vitality.viewmodel.DietViewModel;
 import com.example.vitality.viewmodel.SharedViewModel;
+
+import java.util.List;
 
 public class DashboardFragment extends Fragment{
     private ActivityDashboardBinding binding;
+    private DietViewModel dietViewModel;
     public DashboardFragment(){}
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,12 +30,41 @@ public class DashboardFragment extends Fragment{
         View view = binding.getRoot();
         SharedViewModel model = new
                 ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+
+        // Get the ViewModel instance using AndroidViewModelFactory with the Application context
+        dietViewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(getActivity().getApplication()).create(DietViewModel.class);
+
+
+        //Observe all diet in the database using LiveData and update the UI when there is a change
+        dietViewModel.getAllDiets().observe(getActivity(), new Observer<List<Diet>>() {
+            @Override
+            public void onChanged(@Nullable final List<Diet> diet) {
+                // Create a string to hold all the diet details
+                String allDiet = "";
+                // Loop through each diet and add their details to the string
+                for (Diet temp : diet) {
+                    String daymessage = "Day number: " + temp.dayNum;
+                    String cakeMessage = "Cake today: " + temp.cakeNum;
+                    String beefMessage = "Beef today: " + temp.beefNum;
+                    String combinedMessage = daymessage + "\n" + cakeMessage + "\n" + beefMessage;
+                    String dietDetails = (combinedMessage);
+                    allDiet = allDiet + System.getProperty("line.separator") + dietDetails;
+                }
+                // Set the text of the text view to show all the diet details
+                binding.textViewRead.setText("All data: " + allDiet);
+            }
+        });
+
+
         model.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 binding.textMessage.setText(s);
             }
         });
+
+
+
 
         //return to main page
         binding.returnButton.setOnClickListener(new View.OnClickListener() {
--- Vitality/app/src/main/java/com/example/vitality/fragment/DietlogFragment.java
+++ Vitality/app/src/main/java/com/example/vitality/fragment/DietlogFragment.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.fragment.app.Fragment;
@@ -13,20 +15,50 @@ import androidx.navigation.Navigation;
 
 import com.example.vitality.MainActivity;
 import com.example.vitality.databinding.ActivityDietLogBinding;
+import com.example.vitality.entity.Diet;
+import com.example.vitality.viewmodel.DietViewModel;
 import com.example.vitality.viewmodel.SharedViewModel;
 
+import java.util.List;
 
 
 public class DietlogFragment extends Fragment {
     private ActivityDietLogBinding addBinding;
+
+    // ViewModel instance to access the database operations
+    private DietViewModel dietViewModel;
     public DietlogFragment(){}
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         addBinding = ActivityDietLogBinding.inflate(inflater, container, false);
         View view = addBinding.getRoot();
-        SharedViewModel model = new
+
+        SharedViewModel sharemodel = new
                 ViewModelProvider(getActivity()).get(SharedViewModel.class);
+
+        // Get the ViewModel instance using AndroidViewModelFactory with the Application context
+        dietViewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(getActivity().getApplication()).create(DietViewModel.class);
+
+        //Observe all diet in the database using LiveData and update the UI when there is a change
+        dietViewModel.getAllDiets().observe(getActivity(), new Observer<List<Diet>>() {
+            @Override
+            public void onChanged(@Nullable final List<Diet> diet) {
+                // Create a string to hold all the diet details
+                String allDiet = "";
+                // Loop through each diet and add their details to the string
+                for (Diet temp : diet) {
+                    String daymessage = "Day number: " + temp.dayNum;
+                    String cakeMessage = "Cake today: " + temp.cakeNum;
+                    String beefMessage = "Beef today: " + temp.beefNum;
+                    String combinedMessage = daymessage + "\n" + cakeMessage + "\n" + beefMessage;
+                    String dietDetails = (combinedMessage);
+                    allDiet = allDiet + System.getProperty("line.separator") + dietDetails;
+                }
+                // Set the text of the text view to show all the diet details
+                addBinding.textViewRead.setText("All data: " + allDiet);
+            }
+        });
 
         //add message to the list
         addBinding.addButton.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +76,7 @@ public class DietlogFragment extends Fragment {
                     if (!dayText.equals("")) {
                         day = Integer.parseInt(dayText);
                     }
+
                     if (!cakeText.equals("")) {
                         cake = Integer.parseInt(cakeText);
                     }
@@ -56,13 +89,42 @@ public class DietlogFragment extends Fragment {
                     // Display error message if input is not an integer
                     Toast.makeText(getContext(), "Input must be an integer", Toast.LENGTH_SHORT).show();
                 }
+
+                // check if same day number already exists and throw error message exception
+                List<Diet> dietList = dietViewModel.getAllDiets().getValue();
+                for (Diet temp : dietList) {
+                    if (temp.dayNum == day) {
+                        Toast.makeText(getContext(), "Day number already exists", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                }
+
+
+                // Create a new diet object with the details
+                Diet diet = new Diet(day, cake, beef);
+                // Insert the diet into the database
+                dietViewModel.insert(diet);
+                // Set the text of the text view to show the added diet details
+                addBinding.textViewRead.setText("Added: " + "day: " + day + " cake: " + cake + " beef: " + beef);
+
                 int cakeEnergy = cake * 2000;
                 int beefEnergy = beef * 2000;
                 String daymessage = "Day number: " + day;
                 String cakeMessage = "Cake energy today: " + cakeEnergy + " KJ";
                 String beefMessage = "Beef energy today: " + beefEnergy + " KJ";
                 String combinedMessage = daymessage + "\n" + cakeMessage + "\n" + beefMessage;
-                model.setMessage(combinedMessage);
+                sharemodel.setMessage(combinedMessage);
+            }
+        });
+
+        //Delete button click listener to delete all the diet in the database
+        addBinding.deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Delete all the diet in the database
+                dietViewModel.deleteAll();
+                // Set the text of the text view to show that the database is empty
+                addBinding.textViewRead.setText("All data deleted");
             }
         });
 
